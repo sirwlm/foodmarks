2c5fbd661e134d268a3319a64d651a89d3883ab7
Kevin Leung <kkleung89@gmail.com>
2011-08-07 17:54:04 -0400

    a couple changes to master

--- foodmarks/static/js/tag_editor.js
+++ foodmarks/static/js/tag_editor.js
@@ -18,24 +18,35 @@ function generateTagJsonString(){
     return JSON.stringify(tags);
 }
 
-function createValueTagDict(value){
+function normalize(val){
+    return val.toLowerCase().replace(/\s/g, '');
+}
+
+function createValueTagDict(value, normalizedValue){
     var valueElem = document.createElement('span');
-    valueElem.setAttribute('class', 'tag-value');
+    valueElem.className = 'tag-value';
+    valueElem.setAttribute('title', normalizedValue);
+
+    var remove = document.createElement('span');
+    remove.className = 'tag-remove';
+    remove.appendChild(document.createTextNode('X'));
+    
     valueElem.appendChild(document.createTextNode(value));
+    valueElem.appendChild(remove);
     return {'elem': valueElem, 'original': value};
 }
 
 function addTag(key, value){
     key = $.trim(escapeHTML(key));
     value = $.trim(escapeHTML(value));
-    var normalizedKey = key.toLowerCase().replace(/\s/g, '');
-    var normalizedValue = value.toLowerCase().replace(/\s/g, '');
+    var normalizedKey = normalize(key);
+    var normalizedValue = normalize(value);
 
     if(normalizedKey in tags){
 	if(normalizedValue in tags[normalizedKey]['values']){
 	    $(tags[normalizedKey]['values'][normalizedValue]['elem']).effect('highlight', {}, 3000);
 	}else{
-	    var valueTagDict = createValueTagDict(value);
+	    var valueTagDict = createValueTagDict(value, normalizedValue);
 	    tags[normalizedKey]['values'][normalizedValue] = 
 		valueTagDict;
 	    tags[normalizedKey]['valueCol'].appendChild(valueTagDict['elem']);
@@ -44,12 +55,13 @@ function addTag(key, value){
     }else{
 	var row = document.createElement('tr');
 	var keyCol = document.createElement('td');
+	keyCol.className = 'key';
 	keyCol.appendChild(document.createTextNode(key));
 	row.appendChild(keyCol);
 	var valueCol = document.createElement('td');
 	row.appendChild(valueCol);
 
-	var valueTagDict = createValueTagDict(value);
+	var valueTagDict = createValueTagDict(value, normalizedValue);
 	valueCol.appendChild(valueTagDict['elem']);
 
 	tags[normalizedKey] = {'original': key,
@@ -86,5 +98,26 @@ $(document).ready(function(){
 		    $('#new-value').val('');
 		    break;
 		}
-	    });	      
+	    });
+
+	$('.tag-remove').live('click', function(event){
+		var row = $(this).closest('tr');
+		var key = normalize(row.find('.key').html());
+		var valueElem = $(this).closest('.tag-value');
+		var value = normalize(valueElem.attr('title'));
+		delete tags[key]['values'][value];
+		
+		var valuesLeft = false;
+		for(var val in tags[key]['values']){
+		    valuesLeft = true;
+		    break;
+		}
+		
+		if(!valuesLeft){
+		    document.getElementById('current-tags').removeChild(tags[key]['row']);
+		    delete tags[key];
+		}
+		valueElem.remove();
+		event.stopPropagation();
+	    });
     });
