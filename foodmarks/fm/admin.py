a7e30a93ec63376a0ef9c15d2e60d69684d4d977
Kevin Leung <kkleung89@gmail.com>
2017-02-11 22:31:01 -0800

    minor cleanup

--- foodmarks/fm/admin.py
+++ foodmarks/fm/admin.py
@@ -9,11 +9,13 @@ class RibbonAdmin(admin.ModelAdmin):
     search_fields = ('recipe__title', )
     list_display = ('id', 'recipe', 'user', )
     list_filter = ('user', )
+    raw_id_fields = ('recipe', 'user')
 
 class TagAdmin(admin.ModelAdmin):
     search_fields = ('key', 'value', )
     list_display = ('key', 'value', 'ribbon', )
     list_filter = ('key', )
+    raw_id_fields = ('ribbon', )
 
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ribbon, RibbonAdmin)
